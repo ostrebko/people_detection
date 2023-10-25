a04c0d399fdb42148ce6cdeab73a709ab84c073b
ostrebko <oleg.strebkov@gmail.com>
2023-10-26 01:00:02 +0300

    ch loaded model

--- utils/load_model.py
+++ utils/load_model.py
@@ -1,8 +1,8 @@
 import cv2
 import torch
 
-from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2 as model_4_detection
-from torchvision.models.detection import FasterRCNN_ResNet50_FPN_V2_Weights as weights_4_detection
+from torchvision.models.detection import ssdlite320_mobilenet_v3_large as model_4_detection
+from torchvision.models.detection import SSDLite320_MobileNet_V3_Large_Weights as weights_4_detection
 from torchvision.utils import draw_bounding_boxes
 from torchvision.transforms.functional import to_pil_image
 
@@ -13,8 +13,8 @@ class ModelDetection():
         super().__init__()
         #self.config = config
         self.weights = weights_4_detection.DEFAULT
-        self.model = model_4_detection(weights=self.weights, 
-                                       box_score_thresh=0.9).eval()
+        self.model = model_4_detection(weights=self.weights,
+                                       score_thresh=0.5).eval()
 
     def inference_transforms(self, image):
         
@@ -33,12 +33,17 @@ class ModelDetection():
     
     def get_prediction(self, preproc_img, initial_img):
         
+        with torch.no_grad():
+
         # Use the model and visualize the prediction
-        prediction = self.model(preproc_img)[0]
+            prediction = self.model(preproc_img)[0]
+            #print(prediction)
 
-        labels = [self.weights.meta["categories"][i] for i in prediction["labels"]]
+            #labels = [self.weights.meta["categories"][i] for i in prediction["labels"]]
+            labels = [self.weights.meta["categories"][i] for i in prediction["labels"]]
+            #print(labels)
 
-        box = draw_bounding_boxes(initial_img, 
+            box = draw_bounding_boxes(initial_img, 
                                   boxes=prediction["boxes"],
                                   labels=labels,
                                   colors="red",
@@ -46,24 +51,8 @@ class ModelDetection():
                                   #font_size=30
                                   )
         
-        im = to_pil_image(box.detach())
+            im = to_pil_image(box.detach())
         # im.show()
 
         return im, prediction
-
-
-# model_det = ModelDetection()
-# # model = model_det.model
-# # weights = model_det.weights
-
-# initial_img = cv2.imread('./utils/temp_photo.jpg', cv2.IMREAD_COLOR)
-
-# preproc_img, img_as_tensor = model_det.inference_transforms(initial_img)
-# im, prediction = model_det.get_prediction(preproc_img, img_as_tensor)
-
-# win_name = 'Camera'
-# cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
-
-# cv2.imshow(win_name, initial_img)
-# cv2.waitKey(0)
-# cv2.destroyWindow(win_name)
+    

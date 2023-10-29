f091d5b8cc5551be986fe45438b3484c4434921e
ostrebko <oleg.strebkov@gmail.com>
2023-10-29 22:44:00 +0300

    add config comments

--- utils/camera_acc.py
+++ utils/camera_acc.py
@@ -19,6 +19,7 @@ class getCameraAcc():
 
     def __init__(self, config: dict):
         super().__init__()
+        self.config = config
         self.s = config.s
     
     
@@ -33,7 +34,7 @@ class getCameraAcc():
         win_name = 'Camera'
         cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
-        model_det = ModelDetection()
+        model_det = ModelDetection(self.config)
         #model = model_det.model
         #weights = model_det.weights
         
@@ -43,11 +44,14 @@ class getCameraAcc():
                 break
             frame = cv2.flip(frame, 1) # inverse, around y-axis
             
-            # add some function to work with frame
+            # detection people
             preproc_frame, frame_as_tensor = model_det.inference_transforms(frame)
             frame_with_bbox, prediction = model_det.get_prediction(preproc_frame, 
                                                                    frame_as_tensor)
             
+            # some func to do after detection
+
+
             cv2.imshow(win_name, np.array(frame_with_bbox.convert("RGB")))
             #cv2.waitKey(0) # for debugging step by step
             

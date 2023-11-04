a647a28ae6e9f7fe3fe0c6adab227518ac4c06ac
ostrebko <oleg.strebkov@gmail.com>
2023-11-05 00:20:00 +0300

    ch write log part

--- utils/camera_acc.py
+++ utils/camera_acc.py
@@ -2,7 +2,7 @@ import cv2
 import sys
 import numpy as np
 from utils.load_model import modelDetection
-from utils.write_logs import createLogs
+from utils.write_db import createLogsDB
 
 
 class getCameraAcc():
@@ -35,12 +35,12 @@ class getCameraAcc():
         win_name = 'Camera'
         cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
+        # creating model object
         model_det = modelDetection(self.config)
-        logs = createLogs(self.config)
-        #model = model_det.model
-        #weights = model_det.weights
-        k = 0 #param for debug mode to write num in logs
 
+        # creating logs object to write data detections in database
+        logs_db = createLogsDB(self.config)
+        
         while cv2.waitKey(1) != 27: #Escape
             has_frame, frame = source.read()
             if not has_frame:
@@ -49,23 +49,16 @@ class getCameraAcc():
             
             # detection people
             preproc_frame, frame_as_tensor = model_det.inference_transforms(frame)
-            frame_after_pred, prediction, time_detection = model_det.get_prediction(preproc_frame, 
-                                                                                    frame_as_tensor)
-            
-            # some func to do after detection - think to optimize
+            frame_after_pred, prediction, time_detection, num_peoples = model_det.get_prediction(preproc_frame, 
+                                                                                                 frame_as_tensor)
             if time_detection:
-                k+=1
-                if k%30==0:
-                    logs.write_log_csv(str(time_detection))
-            else:
-                k-=1
-                if k<=0:
-                    k=0
-            print(k)
-            
+                logs_db.add_db(np.datetime_as_string(time_detection, unit='D'), 
+                               np.datetime_as_string(time_detection, unit='s')[11:], 
+                               num_peoples)
+                        
             cv2.imshow(win_name, np.array(frame_after_pred.convert("RGB")))
             #cv2.waitKey(0) # for debugging step by step
         
-        logs.close_log_csv()
+        logs_db.close_db()
         source.release()
         cv2.destroyWindow(win_name)

526c6450b271f019681aa9b1cd04f30ce0a9ffa6
ostrebko <oleg.strebkov@gmail.com>
2023-11-07 19:19:01 +0300

    prework with objs in cropped imgs

--- utils/camera_acc.py
+++ utils/camera_acc.py
@@ -1,5 +1,6 @@
 import cv2
 import sys
+import winsound
 import numpy as np
 from utils.load_model import modelDetection
 from utils.write_db import createLogsDB
@@ -55,9 +56,28 @@ class getCameraAcc():
                 logs_db.add_db(np.datetime_as_string(time_detection, unit='D'), 
                                np.datetime_as_string(time_detection, unit='s')[11:], 
                                num_peoples)
-                        
-            cv2.imshow(win_name, np.array(frame_after_pred.convert("RGB")))
-            #cv2.waitKey(0) # for debugging step by step
+
+                # prework with detected cropped object
+                #for b_box_coords in prediction['boxes']:
+                #    x1 = b_box_coords[0].item()
+                #    x2 = b_box_coords[2].item()
+                #    y1 = b_box_coords[1].item()
+                #    y2 = b_box_coords[3].item()
+                #    print(x1, x2, y1, y2)
+
+                #    print(frame_after_pred.convert("RGB").crop((x1, y1, x2, y2)).size)
+                #    print(np.array(frame_after_pred.convert("RGB").crop((x1, y1, x2, y2))).shape)
+                #    cv2.imshow(win_name, np.array(frame_after_pred.convert("RGB").crop((x1, y1, x2, y2))))
+                
+                # prework with sound notification
+                #freq, duration = 440, 100     
+                #winsound.Beep(freq, duration)
+                
+            
+            #else:
+            cv2.imshow(win_name, np.array(frame_after_pred.convert("RGB"))) # show full image with/without b_box
+            
+            #cv2.waitKey() # for debugging step by step
         
         logs_db.close_db()
         source.release()
